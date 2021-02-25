e4a5609a1e3b1aa978c13a5228b39920110412cc
hulchenko <74687999+hulchenko@users.noreply.github.com>
2021-02-25 16:34:53 -0700

    add node localhost

--- script.js
+++ script.js
@@ -1,1 +1,13 @@
-window.SpeechRecognition = window.SpeechRecognition || window.webkitSpeechRecognition;
+window.SpeechRecognition = window.SpeechRecognition || window.webkitSpeechRecognition; //global variable that lives in browser. First one is for Firefox, second one is for Chrome.
+
+const recognition = new SpeechRecognition();
+recognition.interimResults = true(); //for browser to differentiate speech pauses
+
+let p = document.createElement("p"); //create p element and append it to words div
+const words = document.querySelector(".words");
+words.appendChild(p);
+
+//just like event listener listens for clicks, this one listens to results.
+recognition.addEventListener("results", e => {
+    console.log(e);
+});
